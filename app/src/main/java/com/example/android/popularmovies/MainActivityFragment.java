62735bc4b2e43bf3513585af5e22bebdcbc42fda
sidharth <sidhs.sidharth11@gmail.com>
2015-08-25 12:36:26 +0530

    Add network call to fragment

--- app/src/main/java/com/example/android/popularmovies/MainActivityFragment.java
+++ app/src/main/java/com/example/android/popularmovies/MainActivityFragment.java
@@ -1,11 +1,26 @@
 package com.example.android.popularmovies;
 
-import android.support.v4.app.Fragment;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.GridView;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 
 /**
  * A placeholder fragment containing a simple view.
@@ -16,6 +31,7 @@ public class MainActivityFragment extends Fragment {
     }
 
     public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
 
         setHasOptionsMenu(true);
     }
@@ -30,9 +46,147 @@ public class MainActivityFragment extends Fragment {
         GridView gridView;
 
         gridView = (GridView) rootView.findViewById(R.id.gridview);
-        gridView.setAdapter();
+        gridView.setAdapter(new ImageAdapter(getActivity()));
+
+        FetchMovieTask movieTask = new FetchMovieTask();
+        movieTask.execute();
+
+
+
 
 
         return rootView;
     }
+
+    public class ImageAdapter extends BaseAdapter {
+
+        private Context mContext;
+
+        public ImageAdapter(Context c){
+
+            mContext = c;
+        }
+
+        public int getCount(){
+
+            return mThumbIds.length;
+        }
+
+        public Object getItem(int position){
+
+            return null;
+        }
+
+        public long getItemId(int position){
+
+            return 0;
+        }
+
+        public View getView(int position, View counterView, ViewGroup parent){
+
+            ImageView imageView;
+
+            if(counterView == null){
+
+                imageView = new ImageView(mContext);
+
+
+            }else{
+
+                imageView = (ImageView) counterView;
+            }
+
+
+            Picasso.with(getActivity().getApplicationContext()).load("http://image.tmdb.org/t/p/w185//nBNZadXqJSdt05SHLqgT0HuC5Gm.jpg ").into(imageView);
+
+
+           // imageView.setImageResource(mThumbIds[position]);
+
+            return imageView;
+
+        }
+
+        private Integer[] mThumbIds = {
+
+                R.drawable.ic_launcher,
+                R.drawable.ic_launcher,
+                R.drawable.ic_launcher,
+                R.drawable.ic_launcher,
+
+        };
+
+    }
+
+    public class FetchMovieTask extends AsyncTask<Void, Void, Void>{
+
+        private final String LOG_TAG = FetchMovieTask.class.getSimpleName();
+
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            // These two need to be declared outside the try/catch
+            // so that they can be closed in the finally block.
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a string.
+            String forecastJsonStr = null;
+
+            try {
+
+                URL url = new URL("http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=12af43fb975a79eeadbdff3fdb5f67dc");
+
+                // Create the request to OpenWeatherMap, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                forecastJsonStr = buffer.toString();
+                Log.v(LOG_TAG,"Data fetched " + forecastJsonStr);
+
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in attemping
+                // to parse it.
+                return null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
+            }
+
+
+            return null;
+        }
+
+    }
 }
+
